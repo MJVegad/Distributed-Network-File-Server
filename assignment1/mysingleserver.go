4b02bdf8a4a299965bcfd29fb0ef61d4c31d0708
mihir vegad <mihir.vegad96@gmail.com>
2016-01-13 21:00:44 +0530

    simple tcp server listening on 8080

---
+++ assignment1/mysingleserver.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"time"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	} 
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		} 
+
+		io.WriteString(conn, fmt.Sprint("Hello World\n", time.Now(), "\n"))
+
+		conn.Close()
+
+	}
+
+	
+}
